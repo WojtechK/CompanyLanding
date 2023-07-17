7dddc6632677a3fff4bc25fb49b1ebc8cfeb49c5
WojciechKaczor <wojtek7297@gmail.com>
2023-07-18 00:22:45 +0200

    hamburger mobile menu finalisation

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,7 +1,19 @@
 const header = document.querySelector(".header");
+const menuHamburger = document.querySelector("#menu_checkbox");
+const mobileMenu = document.querySelector(".header-mobile-menu-list");
+const mobileMenuLinks = document.querySelectorAll(".header-mobile-menu-list__item a");
+const mobileMenuCheckbox = document.querySelector('#menu_checkbox');
 
+/**
+ * events listeners
+ */
 window.onscroll = toggleStickyNavbar;
+menuHamburger.addEventListener("click", toggleMobileMenu);
+mobileMenuLinks.forEach(link => link.addEventListener("click", closeMobileMenu));
 
+/**
+ * handlers
+ */
 function toggleStickyNavbar() {
   let headerPosition = header.offsetTop;
   let scroll = window.scrollY;
@@ -11,4 +23,13 @@ function toggleStickyNavbar() {
   } else {
     header.classList.remove("header--sticky");
   }
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("header-mobile-menu-list--open");
+}
+
+function closeMobileMenu() {
+    mobileMenu.classList.remove("header-mobile-menu-list--open");
+    mobileMenuCheckbox.checked = false;
 }
